1c573300648d00967ee6d9ac4ceaf0b093c2590b
Mcandrew <Shane.Mcandrew@klm.com>
2023-03-06 18:40:25 +0100

    projourdyn webpack

--- js/main.js
+++
@@ -1,4 +0,0 @@
-import { Popover } from 'bootstrap';
-
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-[...popoverTriggerList].map((popoverTriggerEl) => new Popover(popoverTriggerEl));
