d02cf684edf1b7dde9fd5b9f9a524651bf4c7168
Mcandrew <Shane.Mcandrew@klm.com>
2023-09-04 09:16:46 +0200

    add dropbox

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('dropbox').addEventListener(
+  'click',
+  () => {
+    window.open('https://www.dropbox.com/oauth2/authorize?client_id=0jblm44r25b76xf&response_type=code', '_blank');
+  },
+);
