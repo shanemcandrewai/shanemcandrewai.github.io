3e094ffd253c4b3f4107ea0c8d6c627f53baba6c
shane <shanedm@protonmail.com>
2023-11-15 08:57:20 +0100

    fix report download filename

--- js/local.js
+++ js/local.js
@@ -3,6 +3,7 @@ export default class Local {
     const anc = document.createElement('a');
     if (typeof filename !== 'undefined') this.filename = filename;
     anc.download = this.filename;
+    document.getElementById('messages').innerText = `Downloading ${this.filename}`;
     anc.href = window.URL.createObjectURL(db.getBlob());
     anc.click();
   }
