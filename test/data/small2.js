35d09c520a105e14e19d129e067974f53f62ffb4
Shane McAndrew <shanedm@protonmail.com>
2025-02-02 11:27:30 +0100

    add small2 files

---
+++ test/data/small2.js
@@ -0,0 +1,23 @@
+export default {
+  wl_updated: '2024-11-05T04:50:38.773Z',
+  shopping: [
+    'bread',
+    'cheese',
+  ],
+  'mk-sim': 'vk-sim',
+  amap: {
+    mk0: 'mv0',
+    arr1: [
+      'e0',
+    ],
+  },
+  bmap: {
+    bmk0: {
+      bmp1: 'bmpv1',
+      bmv1: 'bvv1',
+    },
+    arr1: [
+      'be0',
+    ],
+  },
+};
