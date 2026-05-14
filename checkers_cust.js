c4b1d802afa04eedd9500a61c79e4137c86ad8b8
shane <shanedm@LSCREATIONx>
2026-05-14 17:28:35 +0200

    webrtc

--- checkers_cust.js
+++ checkers_cust.js
@@ -1,5 +1,5 @@
 // checkers_cust.js
 console.log("checkers_cust.js")
-const ctx = document.getElementById("canvas").getContext("2d");
-ctx.font = "50px serif";
-ctx.fillText("Hello world", 50, 90);
+// const ctx = document.getElementById("canvas").getContext("2d");
+// ctx.font = "50px serif";
+// ctx.fillText("Hello world", 50, 90);
