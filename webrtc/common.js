db6cc9a66c55b913a69d662d1515b2200e402650
shane <shanedm@LSCREATIONx>
2026-05-16 10:20:12 +0200

    fixed STUN text

--- webrtc/common.js
+++ webrtc/common.js
@@ -10,7 +10,7 @@ function chatlog(msg) {
 function createPeerConnection(lasticecandidate) {
   configuration = {
     iceServers: [{
-      urls: "stun:stun.stunprotocol.org"}]};
+      urls: stun}]};
   try {
     peerConnection = new RTCPeerConnection(configuration);
   } catch(err) {
